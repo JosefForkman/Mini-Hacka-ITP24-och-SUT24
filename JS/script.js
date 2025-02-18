1e5d79d2665c1b4af6d4c98f6cdbe48397b79dff
sidstedt <daniel.sidstedt@gmail.com>
2025-02-18 15:00:41 +0100

    changed the link to coresponding pages

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,14 @@ function showOption(category) {
     if (category === 'itp') {
         document.getElementById('itp-grid').innerHTML = `
             <div class="expanded">
-                <a href="itp24.html" class="option">ITP24</a>
+                <a href="./HTML/itp24.html" class="option">ITP24</a>
                 <div class="back-button" onclick="restoreBox('itp')">&times;</div>
             </div>
         `;
     } else if (category === 'sut') {
         document.getElementById('sut-grid').innerHTML = `
             <div class="expanded">
-                <a href="sut24.html" class="option">SUT24</a>
+                <a href="./HTML/sut24.html" class="option">SUT24</a>
                 <div class="back-button" onclick="restoreBox('sut')">&times;</div>
             </div>
         `;
